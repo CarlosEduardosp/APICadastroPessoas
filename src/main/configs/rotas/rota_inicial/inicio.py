e1a49f2d7515ba135a59db2e9a40d91e72893158
Carlos Eduardosp <carlos.spadilha@yahoo.com.br>
2024-06-28 01:05:02 -0300

    feat: atualizando deploy automatico com github actions

--- src/main/configs/rotas/rota_inicial/inicio.py
+++ src/main/configs/rotas/rota_inicial/inicio.py
@@ -14,7 +14,8 @@ def inicio():
         "Success": 200,
         "Message": "Api backend Cadastro de Pessoas, Banco PostgreSQL, rodando perfeitamente.",
         "Versão": 1.0,
-        "Info": "Use '(link)/docs' para acessar a documentação no Swagger ",
+        "Info": "Use '(link)/docs' para acessar a documentação no Swagger."
+                "Deploy realizado com github actions, cria imagem docker, envia para docker hub, deploy no render com a imagem do docker hub. ",
         "Dev": "Carlos Eduardo dos S. Padilha"
     }
 
